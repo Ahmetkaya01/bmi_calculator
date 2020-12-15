6ab42fc580bbea2795315519fe17c9c902bdd3f9
ahmetkaya01 <72339442+ahmetkaya01@users.noreply.github.com>
2020-12-16 05:24:48 +0800

    Update
    Second Activity,ScrolView

--- app/src/main/java/com/example/calculatorbmi/MainActivity.java
+++ app/src/main/java/com/example/calculatorbmi/MainActivity.java
@@ -18,6 +18,9 @@ public class MainActivity extends AppCompatActivity {
     private EditText Weight,Height;
     private TextView Answer;
     private Button Next;
+
+
+   //TODO// String putExtra; //todo add string putextra;//
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -28,54 +31,64 @@ public class MainActivity extends AppCompatActivity {
         Height=findViewById(R.id.etHeight);
         Answer=findViewById(R.id.tvAnswer);
         Next=findViewById(R.id.btn2);
+
+        Next.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Next=(Button) findViewById(R.id.btn2);
+                Intent intent = new Intent(MainActivity.this,Results.class);
+                startActivity(intent);
+            }
+        });
+
         //add a radio button, isMale = true, female = false
 
         Calculate.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                String getW=Weight.getText().toString();
-                String getH=Height.getText().toString();
+                String getW = Weight.getText().toString();
+                String getH = Height.getText().toString();
 
-                float Weight=Float.parseFloat(getW);
-                float Height=Float.parseFloat(getH);
+                float Weight = Float.parseFloat(getW);
+                float Height = Float.parseFloat(getH);
 
-                float newH=Height/100;
-                float bmi=Weight/(newH*newH);
+                float newH = Height / 100;
+                float bmi = Weight / (newH * newH);
 
-                Intent intent = new Intent(MainActivity.this,Results.class);
-               // intent.putExtra("bmi", bmi);
-                startActivity(intent);
 
-//                if(bmi<18.5)
-//
-//                {
-//                    Answer.setText("You Are Underweight");
-//                }
-//
-//                else if(bmi>=18.5&&bmi<25){
-//
-//                    Answer.setText("You Are Normal");
-//
-//                }
-//
-//                else Answer.setText("You Are Obese");
 
-            }
 
-            {
-                Next.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        Next=(Button) findViewById(R.id.btn2);
 
-                        Intent intent = new Intent(MainActivity.this,Results.class);
+               if(bmi<18.5)
+               {
+                    Answer.setText("You Are Underweightwefwefwefwefwef");
+                }
 
-                        startActivity(intent);
+              else if(bmi>=18.5&&bmi<25){
+
+                    Answer.setText("You Are Normal ");
+                }
+              else {
+                   Answer.setText("You Are Obese, What should you do if you are overweight or obese?\n" +
+                           "Speak to your doctor about a weight loss program that is right for you. New research shows that a weight loss program should include three components to be successful — diet, exercise, and behavior modification. Your program may include:\n" +
+                           "\n" +
+                           "Diet. A steady weight loss of about one pound a week is the safest way to lose weight. Your doctor can refer you to a registered dietitian if you need help in planning your diet.\n" +
+                           "Regular exercise such as brisk walking, running, swimming, biking, dancing. The amount of exercise needed to lose weight is different for everyone. Talk to your healthcare professional before you begin any new exercise program.\n" +
+                           "Behavior modification techniques such as:\n" +
+                           "Keep a food diary of everything you eat.\n" +
+                           "Shop from a list and do not shop when you're hungry.\n" +
+                           "Take a different route if you usually pass by a tempting fast food place. " +
+
+                           "- If you want to have best advices Click The Next Button");
+               }
+
+
 
                     }
+
+
+
                 });
-            }
-        });
 
 
     }
--- app/src/main/java/com/example/calculatorbmi/Results.java
+++ app/src/main/java/com/example/calculatorbmi/Results.java
@@ -15,4 +15,3 @@ public class Results extends AppCompatActivity {
     }
 }
 
-// HELLO AHMET
