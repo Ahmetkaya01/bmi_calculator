a40c38de37df97a49196f9cefe3fff38d1127b6a
ahmetkaya01 <72339442+ahmetkaya01@users.noreply.github.com>
2021-02-21 21:57:32 +0800

    a

--- app/src/main/java/com/example/calculatorbmi/MainActivity1.java
+++ app/src/main/java/com/example/calculatorbmi/MainActivity1.java
@@ -2,13 +2,140 @@ package com.example.calculatorbmi;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
 
 public class MainActivity1 extends AppCompatActivity {
+
+
+    private Button Happiness;
+    private Button Antistress;
+    private Button Allfooddiet;
+    private Button Selflove;
+    // // // // // // // // // // //
+    private Button Randomworkout;
+    private Button Plankitup;
+    private Button Quickresistance;
+    private Button Bestworkouts;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main1);
+
+        Randomworkout = findViewById(R.id.randomworkout);
+        Plankitup = findViewById(R.id.plankitup);
+        Quickresistance= findViewById(R.id.quickresistance);
+        Bestworkouts = findViewById(R.id.bestworkouts);
+        // // HORIZONTAL // // // // // // // // // // //
+        Happiness = findViewById(R.id.happiness);
+        Antistress = findViewById(R.id.antistress);
+        Allfooddiet = findViewById(R.id.allfooddiet);
+        Selflove = findViewById(R.id.selflove);
+
+        Happiness.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Happiness.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        Happiness = findViewById(R.id.happiness);
+
+                        Button happiniess = (Button) findViewById(R.id.happiness);
+
+                        happiniess.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(MainActivity1.this,happinies.class));
+                            }
+                        });
+
+                        Antistress = findViewById(R.id.antistress);
+
+                        Button antistress = (Button) findViewById(R.id.antistress);
+
+                        antistress.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(MainActivity1.this,Antistress.class));
+                            }
+                        });
+
+                        Allfooddiet = findViewById(R.id.allfooddiet);
+
+                        Button allfooddiet = (Button) findViewById(R.id.allfooddiet);
+
+                        allfooddiet.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(MainActivity1.this,allfooddiet.class));
+                            }
+                        });
+
+                Selflove = findViewById(R.id.selflove);
+
+                Button selflove = (Button) findViewById(R.id.selflove);
+
+                selflove.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity1.this,SelfLove.class));
+                    }
+                });
+
+                Randomworkout = findViewById(R.id.randomworkout);
+
+                Button randomworkout = (Button) findViewById(R.id.randomworkout);
+
+                randomworkout.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity1.this,Randomworkout.class));
+                    }
+                });
+
+                Plankitup= findViewById(R.id.plankitup);
+
+                Button plankitup = (Button) findViewById(R.id.plankitup);
+
+                plankitup.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity1.this,Plankitup.class));
+                    }
+                });
+
+                Quickresistance = findViewById(R.id.quickresistance);
+
+                Button quickresistance = (Button) findViewById(R.id.quickresistance);
+
+                quickresistance.setOnClickListener(new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        startActivity(new Intent(MainActivity1.this,EightQuickresistance.class));
+                    }
+                });
+
+                Bestworkouts = findViewById(R.id.bestworkouts);
+
+                        Button bestworkouts = (Button) findViewById(R.id.bestworkouts);
+
+                        bestworkouts.setOnClickListener(new View.OnClickListener() {
+                            @Override
+                            public void onClick(View v) {
+                                startActivity(new Intent(MainActivity1.this,thebestbuttworkouts.class));
+                            }
+                        });
+
+                    }
+                });
+            }
+        });
+
+
+
     }
 }
--- app/src/main/java/com/example/calculatorbmi/browse.java
+++ app/src/main/java/com/example/calculatorbmi/browse.java
@@ -3,12 +3,21 @@ package com.example.calculatorbmi;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class browse extends AppCompatActivity {
 
+    private Button Browsebtn;
+    private Button Homebtn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_browse);
+
+        Browsebtn = findViewById(R.id.browsebutton);
+        Homebtn = findViewById(R.id.homebutton);
+
+        
+
     }
 }
