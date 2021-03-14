e90eed85ff8967a2ed76cdec1e621750b43bfacd
ahmetkaya01 <72339442+ahmetkaya01@users.noreply.github.com>
2021-03-14 22:12:45 +0800

    test for new_master

--- app/src/main/java/com/example/calculatorbmi/allfooddiet.java
+++ app/src/main/java/com/example/calculatorbmi/allfooddiet.java
@@ -4,7 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 
-public class allfooddiet extends AppCompatActivity {
+public class allfooddiet extends AppCompatActivity { //test for new_master
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
