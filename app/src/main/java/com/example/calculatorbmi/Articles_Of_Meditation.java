85a8432bb69968db796df211eb906661c0099b0d
ahmetkaya01 <72339442+ahmetkaya01@users.noreply.github.com>
2021-03-18 10:58:15 +0800

    Added Articles of Meditation

---
+++ app/src/main/java/com/example/calculatorbmi/Articles_Of_Meditation.java
@@ -0,0 +1,14 @@
+package com.example.calculatorbmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Articles_Of_Meditation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_articles__of__meditation);
+    }
+}
---
+++ app/src/main/java/com/example/calculatorbmi/Types_Of_Meditation.java
@@ -0,0 +1,14 @@
+package com.example.calculatorbmi;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class Types_Of_Meditation extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_types__of__meditation);
+    }
+}
