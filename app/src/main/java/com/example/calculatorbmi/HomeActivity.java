24492ffe02f695b58254a7d4c0d7a4d51cb65ce8
ahmetkaya01 <72339442+ahmetkaya01@users.noreply.github.com>
2021-03-21 23:24:36 +0800

    Browse.xml moved to Home.xml

--- app/src/main/java/com/example/calculatorbmi/HomeActivity.java
+++ app/src/main/java/com/example/calculatorbmi/HomeActivity.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ImageButton;
 
 public class HomeActivity extends AppCompatActivity {
 
@@ -14,7 +15,7 @@ public class HomeActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_home);
 
-        Button happiniess = (Button) findViewById(R.id.Happiness);
+        Button happiniess = (Button) findViewById(R.id.happines);
 
         happiniess.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -24,7 +25,7 @@ public class HomeActivity extends AppCompatActivity {
         });
 
 
-        Button antistress = (Button) findViewById(R.id.Antistress);
+        Button antistress = (Button) findViewById(R.id.antistress);
 
         antistress.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -34,7 +35,7 @@ public class HomeActivity extends AppCompatActivity {
         });
 
 
-        Button allfooddiet = (Button) findViewById(R.id.Allfooddiet);
+        Button allfooddiet = (Button) findViewById(R.id.allfooddiet);
 
         allfooddiet.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -94,27 +95,10 @@ public class HomeActivity extends AppCompatActivity {
         });
 
 
-        Button browsebutton = (Button) findViewById(R.id.browsebutton);
-
-        browsebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(HomeActivity.this, browse.class));
-            }
-        });
-
-        Button homebutton = (Button) findViewById(R.id.homebutton);
-
-        homebutton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-
-                startActivity(new Intent(HomeActivity.this, HomeActivity.class));
-            }
-        });
 
 
-        Button checkyourbmi = (Button) findViewById(R.id.checkyourbmi);
+
+        ImageButton checkyourbmi = (ImageButton) findViewById(R.id.calculator);
 
         checkyourbmi.setOnClickListener(new View.OnClickListener() {
             @Override
